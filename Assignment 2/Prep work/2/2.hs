27c5f70ddbb45b5e06767066b98382fd7dc00e93
haren <lukasz.harezlak@gmail.com>
2013-09-09 15:15:33 +0200

    3/4 of 2 ready

--- Assignment 2/Prep work/2/2.hs
+++ Assignment 2/Prep work/2/2.hs
@@ -1,6 +1,8 @@
 import Week2
 
---time spent on excercise: 
+-- time spent on excercise: 
+
+-------------------------------------------------------------
 
 -- contradiction
 contradiction :: Form -> Bool
@@ -11,17 +13,30 @@ tautology :: Form -> Bool
 tautology f = all (\ v -> eval v f) (allVals f)
 
 -- logical entailment
--- entails :: Form -> Form -> Bool
+-- Note: 1 has 2 as logical consequence if and only if 1 -> 2 is
+-- a tautology.
+entails :: Form -> Form -> Bool
+entails f1 f2 = tautology (Impl f1 f2)
 
 -- logical equivalence
 --equiv :: Form -> Form -> Bool
 
+-------------------------------------------------------------
+
 -- test variables
 testContradictionTrue = Equiv p (Neg p)
 testContradictionFalse = Equiv p (Impl (Neg p) p)
+
 testTautologyTrue = Equiv p (Neg (Neg p))
 testTautologyFalse = Equiv p (Impl p q)
 
+testEntailsTrue_1 = Equiv p q
+testEntailsTrue_2 = Equiv p p
+
+testEntailsFalse_1 = Equiv p q
+testEntailsFalse_2 = Equiv q (Neg q)
+
+-------------------------------------------------------------
 -- testing results
 {-
 *Main> contradiction testContradictionTrue
@@ -38,4 +53,10 @@ True
 *Main> tautology testContradictionTrue
 False
 
+*Main> entails testEntailsTrue_1 testEntailsTrue_2
+True
+*Main> entails testEntailsTrue_1 testEntailsFalse_2
+False
+*Main> entails testEntailsFalse_1 testEntailsFalse_2
+False
 -}
