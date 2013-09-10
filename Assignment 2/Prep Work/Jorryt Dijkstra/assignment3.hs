b03c02e2028dd212c604022d3cba003efb354b90
Jorryt Dijkstra <jjdijkstra1@gmail.com>
2013-09-10 18:57:55 +0200

    added equiv test, started on entails test

--- Assignment 2/Prep Work/Jorryt Dijkstra/assignment3.hs
+++ Assignment 2/Prep Work/Jorryt Dijkstra/assignment3.hs
@@ -3,19 +3,19 @@ module AssignmentCNF where
 import Week2
 
 cnf :: Form -> Form
-cnf x
-	| (x == Cnj [p, q]) = Cnj[cnf p, cnf q]
-	| (x == Dsj [p, q]) = (dist (cnf p) (cnf q))
-	| otherwise = x
+cnf (Cnj x) = Cnj[cnf (head x), subcnf (tail x)]
+cnf (Dsj x) = Cnj[cnf p, cnf q]
+cnf (Prop x) = Prop x
+cnf (x) = error "invalid input"
+
+subcnf :: [Form] -> Form
+subcnf (x:xs) = (cnf x : subcnf xs)
 
 
 dist :: Form -> Form -> Form
-dist x y 
-	| (x == Cnj [p, q]) = Cnj[dist p y, dist q y]
-	| (y == Cnj [p, q]) = Cnj[dist x p, dist x q]
-	| otherwise = Dsj[x, y]
+dist (Cnj x) y = Cnj[dist p y, dist q y]
+dist x (Cnj y) = Cnj[dist x p, dist x q]
+dist x y = Dsj[x, y]
 
 
-testFrm = Dsj[(Cnj[p,q], r]
-
-
+testFrm = Dsj[Cnj[p, q], r]
