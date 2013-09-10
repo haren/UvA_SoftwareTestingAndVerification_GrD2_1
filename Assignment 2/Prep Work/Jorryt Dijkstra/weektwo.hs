a6b86e74209f240ed5b99828f8c1cd2b6d0d9b32
Jorryt Dijkstra <jjdijkstra1@gmail.com>
2013-09-10 10:31:33 +0200

    Assignment 1 finished tests

--- Assignment 2/Prep Work/Jorryt Dijkstra/weektwo.hs
+++ Assignment 2/Prep Work/Jorryt Dijkstra/weektwo.hs
@@ -1,11 +1,12 @@
 -- J. Dijkstra
 -- Software Testing - Lab Assignment 2
 import Data.List
+import Data.Array.IO
 
-data Shape = NoTriangle | Equilateral | Isosceles  | Rectangular | Other deriving (Eq, Show)
+data Shape = NoTriangle | Equilateral | Isosceles | Rectangular | Other deriving (Eq, Show)
 triangle :: Integer -> Integer -> Integer -> Shape
 triangle x y z
-		| (x <= 0 || y <= 0 || z <= 0) = error "Invalid input on coordinates"
+		| (x <= 0 || y <= 0 || z <= 0) = NoTriangle -- or error "Invalid input on coordinates" would also be fine
 		| not (triangleDefinition x y z) = NoTriangle
 		| (x == y && y == z) = Equilateral
 		| (x == y || y == z || x == z) = Isosceles
@@ -29,3 +30,50 @@ inputPythagoras (a:as) (b:bs)
 			c = sqrt((fromIntegral) (a^2 + b^2))
 			pythagorasTail = (inputPythagoras as bs)
 			isInteger n = (floor n == ceiling n)
+
+testEquilateral :: Bool
+testEquilateral = inputEquilateral s s s
+		where s = [1..1000000]
+
+inputEquilateral :: [Integer] -> [Integer] -> [Integer] -> Bool
+inputEquilateral [] _ _ = True
+inputEquilateral _ [] _ = True
+inputEquilateral _ _ [] = True
+inputEquilateral (x:xs) (y:ys) (z:zs) = (triangle x y z == Equilateral) && (inputEquilateral xs ys zs)
+
+testNoTriangle :: Bool
+testNoTriangle = inputNoTriangle s s s && (inputNoTriangle [-2,5,7,3,0,20] [1,0,-3,7,1,10] [4,5,6,-1,9,0])
+		where s = [-100000..0]
+inputNoTriangle [] _ _ = True
+inputNoTriangle _ [] _ = True
+inputNoTriangle _ _ [] = True
+inputNoTriangle (x:xs) (y:ys) (z:zs) = (triangle x y z == NoTriangle) && (inputNoTriangle xs ys zs)
+
+testIscosceles :: Bool
+testIscosceles = inputIscosceles x x y && inputIscosceles x y x && inputIscosceles y x x
+	where 
+		x = [100..100000]
+		y = (map (50+) x)
+
+inputIscosceles :: [Integer] -> [Integer] -> [Integer] -> Bool
+inputIscosceles [] [] [] = True
+inputIscosceles (x:xs) (y:ys) (z:zs) = (triangle x y z == Isosceles) && (inputIscosceles xs ys zs)
+
+testOther :: Bool
+testOther = inputOther x y z
+	where
+		x = [1..100001]
+		y = [2..100002]
+		z = [3..100003]
+
+inputOther :: [Integer] -> [Integer] -> [Integer] -> Bool
+inputOther [] [] [] = True
+inputOther (x:xs) (y:ys) (z:zs)
+	| (c) = (t == Other) && tail
+	| otherwise = tail
+	where 
+		t = triangle x y z
+		c = (t /= NoTriangle && t /= Equilateral && t /= Isosceles && t /= Rectangular)
+		tail = inputOther xs ys zs
+
+testTriangle = testEquilateral && testIscosceles && testPythagoras && testNoTriangle && testOther
