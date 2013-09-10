6da7bdaa001fcd8ab7384b2828d35ab195af0c6a
haren <lukasz.harezlak@gmail.com>
2013-09-10 12:46:54 +0200

    working on 3rd excercise (Jorryt's folder)

--- Assignment 2/PrepWork/Jorryt Dijkstra/assignment3.hs
+++ Assignment 2/PrepWork/Jorryt Dijkstra/assignment3.hs
@@ -1,13 +1,18 @@
 module AssignmentCNF where
 
 import Week2
-
+{-
 cnf :: Form -> Form
 cnf x
-	| (x == Cnj [p, q]) = Cnj[cnf p, cnf q]
-	| (x == Dsj [p, q]) = (dist (cnf p) (cnf q))
+	| (x == Cnj x) = error "cnf cnj"
+	| (x == Dsj [p, q]) = error "cnf dsj"
 	| otherwise = x
-
+-}
+cnf2 :: Form -> Form
+cnf2 x = cnf3 x where 
+	cnf3 (Cnj x) = Cnj[cnf2(head x), cnf2 (tail x)]
+	cnf3 (Dsj x) = (dist (cnf2 (head x)) (cnf2 (tail x)))
+	cnf3 x = x
 
 dist :: Form -> Form -> Form
 dist x y 
@@ -16,4 +21,18 @@ dist x y
 	| otherwise = Dsj[x, y]
 
 
+testCnj = Cnj[p,q]
+testDsj = Dsj[p,q]
+testFrm = Dsj[(Cnj[p,q]), r]
 
+{-
+	| (x == Cnj [p, q]) = Cnj[cnf p, cnf q]
+	| (x == Dsj [p, q]) = (dist (cnf p) (cnf q))
+	| otherwise = x
+
+
+	| (x == Cnj [p, q]) = Cnj[dist p y, dist q y]
+	| (y == Cnj [p, q]) = Cnj[dist x p, dist x q]
+	| otherwise = Dsj[x, y]
+
+-}
