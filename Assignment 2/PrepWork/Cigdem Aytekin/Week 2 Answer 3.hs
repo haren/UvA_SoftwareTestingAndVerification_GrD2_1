93ea554b80806b7c3ff053b21cd6553d23ff021a
caytekin <cigdemaytekin_872@hotmail.com>
2013-09-12 09:10:44 +0200

    Third question's answer, compiles OK, not tested

--- Assignment 2/PrepWork/Cigdem Aytekin/Week 2 Answer 3.hs
+++ Assignment 2/PrepWork/Cigdem Aytekin/Week 2 Answer 3.hs
@@ -1,20 +1,30 @@
+module AssignmentCNF where
 
-module Module3 where
 import Week2
 
-
 cnf :: Form -> Form
-cnf x
-	| x == (Prop p) 	= Prop p
-	| x == (Cnj [p, q]) = Cnj [cnf p, cnf q]
-	| x == (Cnj [p, q]) = Cnj [cnf p, cnf q]
-	| otherwise 		= error "This is an error!"
+cnf (Prop x) 		= Prop x
+cnf (Cnj fs) 		= Cnj (map cnf fs)
+cnf (Dsj f) 		= dist fh (distList ft)  
+					where
+					 	  fh = head f		
+						  ft = tail f
+
+distList :: [Form] -> Form 
+distList [f1, f2]		= dist f1 f2
+distList (fh:ft)		= dist fh (distList ft) 
+
 
 
 dist :: Form -> Form -> Form
-dist x y
-	| (x == Cnj [p, q]) = Cnj [dist p y, dist q y]
-	| (y == Cnj [r, s]) = Cnj [dist x r, dist x s]
-	| otherwise = Dsj [x, y]
-	
+dist f (Cnj g) = Cnj [dist f gh, dist f gs]
+					where
+					 	  gh = head g		
+						  gs = last g
+dist (Cnj f) g = Cnj [dist fh g, dist ft g]
+					where
+					 	  fh = head f		
+						  ft = last f
+dist f g 	  = Dsj [f, g]
+	 
 
