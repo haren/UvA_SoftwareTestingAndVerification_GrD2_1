6bf4a60f5b08f9cfe3b9d72d7b07f8b7690d3e1a
haren <lukasz.harezlak@gmail.com>
2013-09-15 12:28:47 +0200

    LH 1 excercise (similar solution to cigdem's)

---
+++ Assignment 2/PrepWork/Lukasz Harezlak/1.hs
@@ -0,0 +1,15 @@
+import Week2
+import Data.List
+
+data Shape = NoTriangle | Equilateral | Isosceles | Recatngular | Other deriving(Eq,Show)
+
+triangle :: Integer -> Integer -> Integer -> Shape
+triangle a b c = testTriangle(sort [a,b,c])
+
+testTriangle :: [Integer] -> Shape
+testTriangle [a,b,c] 	| (a <= 0 || b <= 0 || c <= 0) = NoTriangle
+						| ((a+b) <= c) = NoTriangle
+						| ((a==b) && (b==c)) = Equilateral
+						| (a==b || b==c || a==c) = Isosceles						
+						| ((a^2)+(b^2) == (c^2)) = Recatngular
+						| otherwise = Other
