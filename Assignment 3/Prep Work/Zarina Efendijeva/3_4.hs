12dc6f396f40e967f02c36d679a405d34875f944
Zarinka <efendijeva@gmail.com>
2013-09-18 10:28:32 +0200

    Exercise 4 - isPermutation works! (added empty list check aswell)

--- Assignment 3/Prep Work/Zarina Efendijeva/3_4.hs
+++ Assignment 3/Prep Work/Zarina Efendijeva/3_4.hs
@@ -5,6 +5,9 @@ import Data.List
 -- HELL YEAH!!!!!
 
 isPermutation :: Eq a => [a] -> [a] -> Bool
+isPermutation [] [] = True -- if boss lists empty / then true, since they are equal? Can be change to false if you think so guys.
+isPermutation [] _ = False -- 
+isPermutation _ [] = False
 isPermutation a b =
   let equal x = (b == x)
   in null(filter equal(permutations(a))) /= True
@@ -15,5 +18,6 @@ test2 = isPermutation [3,4,2] [3,2,4]  --
 test3 = isPermutation [3,4,2,9] [9,2,4,3]
 
 test4 = isPermutation [3,4,2,9] [9,2,4,9]  --False
+
 
 --test3 = sort(permutations[4,2,1])
