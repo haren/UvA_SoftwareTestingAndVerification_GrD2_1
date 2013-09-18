114966a5aa2c10d46b7af1b36be16df94c308b68
Zarinka <efendijeva@gmail.com>
2013-09-18 09:57:31 +0200

    Exercise 4 - isPermutation WORKS!

--- Assignment 3/Prep Work/Zarina Efendijeva/3_4.hs
+++ Assignment 3/Prep Work/Zarina Efendijeva/3_4.hs
@@ -1,19 +1,19 @@
 module Week3_4 where
 
 import Data.List
-import Data.Ord 
 
---quicksort :: (Ord a) => [a] -> [a]
---quicksort a = sort a
+-- HELL YEAH!!!!!
 
 isPermutation :: Eq a => [a] -> [a] -> Bool
-isPermutation a b = (a ==  b)
-				--where a = (quicksort a)
-
+isPermutation a b =
+  let equal x = (b == x)
+  in null(filter equal(permutations(a))) /= True
 
 
 test = isPermutation [3,4,2] [3,4,2] -- True
-test2 = isPermutation [3,4,2] [3,2,4]  -- False, has to be true after sorting
+test2 = isPermutation [3,4,2] [3,2,4]  -- 
+test3 = isPermutation [3,4,2,9] [9,2,4,3]
 
-test3 = sort([4,2,1])
-test4 = quicksort([4,2,1])
+test4 = isPermutation [3,4,2,9] [9,2,4,9]  --False
+
+--test3 = sort(permutations[4,2,1])
