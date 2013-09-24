da53e10432533350d7996d172718b89b57c0dd9a
haren <lukasz.harezlak@gmail.com>
2013-09-24 18:25:40 +0200

    LH so far

--- Assignment 4/Prep Work/Lukasz Harezlak/4.hs
+++ Assignment 4/Prep Work/Lukasz Harezlak/4.hs
@@ -7,6 +7,7 @@ import Data.List
 
 
 -- 4.2 
+-- 2 hours
 getRandomInt :: IO Int
 getRandomInt = getStdRandom(random)
 
@@ -27,35 +28,84 @@ getRandomOrderedList x min max = do
 	as <- getRandomOrderedList (x-1) (a+1) (max+1)
 	return (a:as)
 
-getListOfRandomOrderedLists :: Int -> IO [[Int]]
-getListOfRandomOrderedLists 0 = return []
-getListOfRandomOrderedLists x = do
-	numberOfListMembers <- getRandomIntInRange 0 10
-	a <- getRandomOrderedList (numberOfListMembers) 0 100
-	as <- getListOfRandomOrderedLists (x-1)
-	return (a:as)
-	
+getRandomSet :: Int -> Int -> Int -> IO (Set Int)
+getRandomSet a min max = do
+	list <- (getRandomOrderedList a min max)
+	return (list2set list)
 
-getRandomSet :: Set a
-getRandomSet = do
-	list <- (getListOfRandomOrderedLists 2)
-	return Set (list)
-	
-{-
--- getRandomListOfLists = [[]]
---getRandomListOfLists :: [[a]]
---getRandomListOfLists = do
---	numberOfSublists <- getRandomIntInRange 0 10	
---	return [[numberOfSublists]]
---	--return (getRandomLists numberOfSublists)
-
-----getRandomLists :: Int -> [[a]]
-----getRandomLists x = []
--}
 -- 4.3
+-- 2 hours
 
+setIntersection :: (Ord a) => Set a -> Set a -> Set a 
+setIntersection (Set[]) b = Set[]
+setIntersection a (Set[]) = Set[]
+setIntersection (Set(xs))(Set(ys)) = list2set ([x | x <- xs, y <- ys, x==y])
+
+setUnion :: (Ord a) => Set a -> Set a -> Set a 
+setUnion (Set [])     set2  =  set2
+setUnion (Set (x:xs)) set2  = 
+   insertSet x (setUnion (Set xs) set2)
+
+setDifference :: (Ord a) => Set a -> Set a -> Set a
+setDifference (Set(xs)) (Set(ys)) = list2set(filter (\x -> not (elem x ys)) xs)
+
+-- testing
+
+testSetOperations :: IO Bool
+testSetOperations = do
+	set1 <- getRandomSet 10 0 100
+	set2 <- getRandomSet 10 0 100
+	let intersection = setIntersection set1 set2
+	let correctSubsets = subSet intersection set1 && subSet intersection set2
+	list1 <- getRandomOrderedList 5 0 1000
+	list2 <- getRandomOrderedList 5 0 1000
+	let set3 = list2set list1
+	let set4 = list2set list2
+	let listIntersect = [x | x <- list1, y <- list2, x==y]
+	let setIntersect = setIntersection set3 set4
+	let setIntersectfromListIntersect = list2set listIntersect
+	let listUnion = list1 ++ list2
+	let setUnion_ = setUnion set3 set4
+	let setUnionfromListUnion = list2set listUnion
+	let listDifference = filter (\x -> not (elem x list2)) list1
+	let setDifference_ = setDifference set3 set4
+	let setDifferenceFromListDifference = list2set listDifference
+	-- check if operation performed on lists			
+	-- is equal to operation performed on sets 
+	-- A == B when A is subset of B and B is subset of A	
+	return (correctSubsets 
+		&& (subSet setIntersect setIntersectfromListIntersect) 
+		&& (subSet setIntersectfromListIntersect setIntersect)
+		&& (subSet setUnion_ setUnionfromListUnion)
+		&& (subSet setUnionfromListUnion setUnion_)
+		&& (subSet setDifference_ setDifferenceFromListDifference)
+		&& (subSet setDifferenceFromListDifference setDifference_))
+		
 
 -- 4.4
 
+type Rel a = [(a,a)]
+
+infixr 5 @@
+
+(@@) :: Eq a => Rel a -> Rel a -> Rel a
+r @@ s =
+	nub [ (x,z) | (x,y) <- r, (w,z) <- s, y == w ]
+
+-- transitive closure of a relation
+--trClos :: Ord a => Rel a -> Rel a
+--trClos [] = []
+--trClos (xs) = getElements xs
+
+-- return a list of all elements
+-- build tuples from this list
+
+
+--getElements :: Ord a => Rel a -> [Int]
+--getElements [] = []
+--getElements (x:xs) = (fst x) ++ (snd x) ++ (getElements xs)
+
+testRel = [(1,2), (1,3)]
+testRel2 = [(2,4), (3,7)]
 
 -- 4.5
