41b2000f15556f2d1fcef42e840ec8e7b0269643
Jorryt Dijkstra <jjdijkstra1@gmail.com>
2013-10-08 18:06:19 +0200

    Finished… started on bonus

--- Assignment 6/Prep Work/Jorryt Dijkstra/weeksix.hs
+++ Assignment 6/Prep Work/Jorryt Dijkstra/weeksix.hs
@@ -3,6 +3,7 @@ module Main
 where
 import Data.List
 import Data.Bits
+import Control.Monad
 import System.Random
 import System.TimeIt
 import Week6
@@ -60,8 +61,13 @@ main = do
 composites :: [Integer]
 composites = [ n | n <- [4..], not $ isPrime n ]
 
--- The first composite integer to be found prime was 4, 
+carmichael :: [Integer]
+carmichael = [ (6*k+1)*(12*k+1)*(18*k+1) | k <- [2..], isPrime (6*k+1), isPrime (12*k+1), isPrime (18*k+1) ]
+
+-- The first composite integer to be found prime was 4
+testFermat :: IO ()
 testFermat = do
+  accuracy <- getRandomIntInRange 10 100 -- amount of tests to do for each primality check (determines the accuracy obviously)
   amount <- getRandomIntInRange 1000 10000
   staticTest <- prime_test_F 1
   staticTest2 <- prime_test_F 2
@@ -71,18 +77,21 @@ testFermat = do
   putStrLn ("1: " ++ show staticTest)
   putStrLn ("2: " ++ show staticTest2)
   putStrLn ("3: " ++ show staticTest3)
-  putStrLn ("Dynamic Tests, the numbers from the first " ++ show amount ++ " composite numbers that pass the test:")
+  putStrLn ("Testing the numbers from the first " ++ show amount ++ " composite and the carmichael numbers for primality")
+  dynamicTest <- repeatFermatCheck accuracy amount composites
+  -- carmichaelTest <- repeatFermatCheck accuracy amount carmichael
   
-  dynamicTest <- repeatFermatCheck 40 amount composites
+  putStrLn ("Composite numbers found as false positives: " ++ show dynamicTest)
+  -- putStrLn ("Carmichael numbers found as false positives: " ++ show carmichaelTest)
   putStrLn "End of test"
-  
-  putStrLn ("Composite numbers found: " ++ show dynamicTest)
+
   where
         repeatFermatCheck 0 n (x:xs) = return []
         repeatFermatCheck a n xs = do
           result <- testFermat' a n xs
           testTail <- repeatFermatCheck (a-1) n xs
           return ((a, result) : testTail)
+
         testFermat' _ 0 _ = return []
         testFermat' a n (x:xs) = do
           result <- primeF a x
@@ -94,9 +103,49 @@ testFermat = do
           if result
           then return (x : testTail)
           else return testTail
-          
 
+-- More accurate for carmichael numbers
+testMillerRabin :: IO ()
+testMillerRabin = do
+  accuracy <- getRandomIntInRange 10 100 -- amount of tests to do for each primality check (determines the accuracy obviously)
+  amount <- getRandomIntInRange 10 20
+  putStrLn ("Testing MR test with " ++ show amount ++ " carmichael numbers, with initial accuracy " ++ show accuracy ++ " and loosening it over time")
+  results <- repeatMillerCheck accuracy (take amount carmichael)
+  putStrLn("Results of false positives (format: (accuracy, [prime numbers]):\n " ++ show results)
+  where 
+        repeatMillerCheck 0 (x:xs) = return []
+        repeatMillerCheck a xs = do
+          result <- testMillerRabin' a xs
+          testTail <- repeatMillerCheck (a-1) xs
+          return ((a, result) : testTail)
 
+        testMillerRabin' 0 _ = return []
+        testMillerRabin' a xs = do
+          results <- mapM (\x -> primeMR a x) xs
+          let zippedResults = zip xs results
+              outputResults = map (fst) (filter (\(_,y) -> y) zippedResults)
+          mapM_ (\(x,y) -> putStrLn $ show x ++ ": " ++ show y) zippedResults
 
+          testTail <- testMillerRabin' (a-1) xs
+          return (outputResults ++ testTail)
 
+-- Parameter is the amount of primes to consider
+generateMersennePrimes :: Int -> IO [Integer]
+generateMersennePrimes primeThreshold = do
+  let mersennes = [ ((2^p)-1) | p <- take primeThreshold primes ]
+  results <- filterM (\x -> primeMR 1 x) mersennes
+  return results
+  
+generatePublicKeyPair :: IO (Int, Int)
+generatePublicKeyPair = do
+  p <- generateRandomPrime
+  p2 <- generateRandomPrime
+  return (p, p2)
 
+generateRandomPrime :: IO Int
+generateRandomPrime = do
+  p <- getRandomIntInRange 100000000000000 900000000000000
+  result <- primeMR 3 (fromIntegral p)
+  if ((not) result)
+  then generateRandomPrime
+  else return p
