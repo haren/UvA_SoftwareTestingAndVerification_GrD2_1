c57a8fbc8d3892276508f704d4df832d4b7864c3
Zarinka <efendijeva@gmail.com>
2013-10-13 12:23:48 +0200

    Exercise: 6.7

---
+++ Assignment 6/Prep Work/Zarina Efendijeva/Assignment6_7.hs
@@ -0,0 +1,25 @@
+module Lab6_7
+where
+
+import Week6	
+import System.Random
+import Control.Monad 
+
+-- take a large prime p, and use the Miller-Rabin algorithm
+-- to check whether 2^p-1 is also prime
+
+-- to generate random prime between x and y
+randomInt :: Integer -> Integer -> IO Integer
+randomInt x y = getStdRandom(randomR(x,y))
+
+test = randomInt 1000 9000
+
+mersennePrime :: Int -> IO [Integer]
+mersennePrime x = do
+  let merNr = [ ((2^p)-1) | p <- take x primes ]
+  results <- filterM (\x -> primeMR 1 x) merNr
+  return results
+
+test2 = mersennePrime 31
+
+
